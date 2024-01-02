a50c5c5bbfec9d31151f4c4766baceb081d2f06c
Erfun Ghodoosi <erfunghodoosi@gmail.com>
2024-01-02 21:15:12 +0330

    useClickOutside added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { useRef } from 'react'
+import useClickOutside from './hooks/useClickOutside'
+
 const App = () => {
-  return <div>App</div>
+  const buttonRef = useRef(null)
+
+  useClickOutside(buttonRef, () => {
+    console.log('clicked outside!')
+  })
+
+  return (
+    <div>
+      <button ref={buttonRef}> the button</button>
+    </div>
+  )
 }
 
 export default App
