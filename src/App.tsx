ee4258ad328bc67e7dd1eaa197de677ff3862f69
Erfun Ghodoosi <erfunghodoosi@gmail.com>
2024-01-02 23:23:06 +0330

    useOrientation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
 import useIsFirstRender from './hooks/useIsFirstRender'
+import useOrientation from './hooks/useOrientation'
 
 const App = () => {
   const [state, setState] = useState(10)
   const isFirstRender = useIsFirstRender()
+  const { isLandscape, isPortrait, state: orientation } = useOrientation()
+  console.log(isLandscape, isPortrait)
   return (
     <div>
-      App {isFirstRender ? 'yes' : 'no'}
+      App {isFirstRender ? 'yes' : 'no'} - {orientation}
       <button onClick={() => setState(pre => pre + 1)}>click {state}</button>
+      <ul>
+        <li>landscape: {isLandscape && 'OK'}</li>
+        <li>portrait: {isPortrait && 'OK'}</li>
+      </ul>
     </div>
   )
 }
