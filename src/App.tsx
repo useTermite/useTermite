fdd6ad8d19629b18ef51dc7a72df8fe57376bfcc
Erfun Ghodoosi <erfunghodoosi@gmail.com>
2024-01-02 22:32:16 +0330

    useKeyPress

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import useMediaQuery from './hooks/useMediaQuery'
+import { useState } from 'react'
+import useKeyPress from './hooks/useKeyPress'
 
 const App = () => {
-  const isSmallDevice = useMediaQuery('only screen and (max-width : 768px)')
-  const isMediumDevice = useMediaQuery('only screen and (min-width : 769px) and (max-width : 992px)')
-  const isLargeDevice = useMediaQuery('only screen and (min-width : 993px) and (max-width : 1200px)')
-  const isExtraLargeDevice = useMediaQuery('only screen and (min-width : 1201px)')
+  const [show, setShow] = useState(true)
 
-  return (
-    <div>
-      {isSmallDevice && <p>You are on a small device.</p>}
-      {isMediumDevice && <p>You are on a medium device.</p>}
-      {isLargeDevice && <p>You are on a large device.</p>}
-      {isExtraLargeDevice && <p>You are on an extra large device.</p>}
-    </div>
-  )
+  useKeyPress('Escape', () => {
+    setShow(false)
+  })
+
+  useKeyPress('Enter', () => {
+    setShow(true)
+  })
+
+  if (show) return <div>App</div>
 }
+
 export default App
