35d64b853f0b757bb4d443613ba43b6e18d5dcdc
Erfun Ghodoosi <erfunghodoosi@gmail.com>
2024-01-03 08:23:47 +0330

    some whole changes

---
+++ index.ts
@@ -0,0 +1,20 @@
+import useClickOutside from './hooks/useClickOutside'
+import useNetworkState from './hooks/useNetworkState'
+import { useCookies } from './hooks/useCookies'
+import useOrientation from './hooks/useOrientation'
+import useDevice from './hooks/useDevice'
+import useMediaQuery from './hooks/useMediaQuery'
+import useKeyPress from './hooks/useKeyPress'
+import useLocation from './hooks/useLocation'
+import useIsFirstRender from './hooks/useIsFirstRender'
+export {
+  useClickOutside,
+  useNetworkState,
+  useCookies,
+  useOrientation,
+  useDevice,
+  useMediaQuery,
+  useKeyPress,
+  useLocation,
+  useIsFirstRender
+}
