93c95d242eee6d4742a60f4b65d6d92a105a6298
Erfun Ghodoosi <erfunghodoosi@gmail.com>
2024-01-05 19:35:18 +0330

    useEventListener eventDom type

--- hooks/useEventListener.ts
+++ hooks/useEventListener.ts
@@ -2,11 +2,12 @@ import { useEffect, RefObject } from 'react'
 
 // Define the type for the callback function
 type EventCallback = (event: Event) => void
+type DOMEventType = keyof GlobalEventHandlersEventMap
 
 // Custom hook to add and remove event listeners on a DOM element
 const useEventListener = (
   ref: RefObject<HTMLElement>, // A ref object pointing to the DOM element
-  event: string, // The name of the event to listen for (e.g., 'click', 'mouseover')
+  event: DOMEventType, // The name of the event to listen for (e.g., 'click', 'mouseover')
   callback: EventCallback // The function to execute when the event is triggered
 ): void => {
   useEffect(() => {
