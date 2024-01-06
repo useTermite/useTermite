04791281c14b7a9d0bae6cea0cc646739ca1c87c
sina-byn <sina.sina.bayan@protonmail.com>
2024-01-06 10:59:18 +0330

    useForceUpdate return value updated

--- hooks/useForceUpdate.ts
+++ hooks/useForceUpdate.ts
@@ -1,11 +1,17 @@
 import { useState } from 'react'
 
-const useForceUpdate = (): Function => {
+// * types
+type UseForceUpdateReturn = [Function, boolean]
+
+const useForceUpdate = (): UseForceUpdateReturn => {
   const [_, setState] = useState<boolean>(true)
 
   const forceUpdate = () => setState(prev => !prev)
 
-  return forceUpdate
+  // * note that the state is only supposed to be used inside of the
+  // * dependency array of a useEffect or such and the function is what
+  // * we mostly use that's why it's exported as the first element
+  return [forceUpdate, _]
 }
 
 export default useForceUpdate
