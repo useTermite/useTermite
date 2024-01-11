8adcb81e82cbb6c85f6575353d326301781f073d
Erfun Ghodoosi <erfunghodoosi@gmail.com>
2024-01-11 10:55:35 +0330

    copy paste handlers names

--- hooks/useCopyPaste.ts
+++ hooks/useCopyPaste.ts
@@ -7,7 +7,7 @@ interface CopyPasteFunctions {
 // Custom hook named useCopyPaste that returns CopyPasteFunctions.
 const useCopyPaste = (): CopyPasteFunctions => {
   // Function to copy text to the clipboard using the Clipboard API.
-  const copyToClipboard = async (text: string): Promise<void> => {
+  const copyHandler = async (text: string): Promise<void> => {
     try {
       await navigator.clipboard.writeText(text);
     } catch (error) {
@@ -16,7 +16,7 @@ const useCopyPaste = (): CopyPasteFunctions => {
   };
 
   // Function to retrieve text from the clipboard using the Clipboard API.
-  const pasteFromClipboard = async (): Promise<string> => {
+  const pasteHandler = async (): Promise<string> => {
     try {
       const text = await navigator.clipboard.readText();
       return text;
@@ -27,7 +27,7 @@ const useCopyPaste = (): CopyPasteFunctions => {
   };
 
   // Return an object containing the copy and paste functions.
-  return { copy: copyToClipboard, paste: pasteFromClipboard };
+  return { copy: copyHandler, paste: pasteHandler };
 };
 
 // Export the useCopyPaste hook.
